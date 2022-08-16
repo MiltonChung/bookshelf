a78e1862abcfb3dceb57c72faf6041da7c5116a6
Milton Chung <miltonjchung@gmail.com>
2022-08-15 22:03:00 -0700

    routing

--- src/setupProxy.exercise.js
+++ src/setupProxy.exercise.js
@@ -1,4 +1,9 @@
 // 🐨 you don't need to do anything in this file for the exercise. This is
 // just here for the extra credit. See the instructions for more info.
+const proxy = app => {
+  app.get(/^\/$/, (_, res) => {
+    res.redirect('/discover')
+  })
+}
 
-module.exports = () => {}
+module.exports = proxy
