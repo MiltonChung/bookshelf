6f7bc8c9c0ae7841a5aa381d2225ebc669af7f68
Milton Chung <miltonjchung@gmail.com>
2022-09-10 22:28:07 -0700

    Performance

--- src/components/profiler.exercise.js
+++ src/components/profiler.exercise.js
@@ -1,1 +1,45 @@
-// this is for extra credit
+import React from 'react'
+import {client} from 'utils/api-client'
+
+let queue = []
+
+const sendProfileQueue = () => {
+  if (queue.length === 0) {
+    return Promise.resolve({success: true})
+  }
+  const queueToSend = [...queue]
+  queue = []
+  return client('profile', {data: queueToSend})
+}
+
+setInterval(sendProfileQueue, 5000)
+
+const Profiler = ({phases, metadata, ...props}) => {
+  const reportProfile = (
+    id,
+    phase,
+    actualDuration,
+    baseDuration,
+    startTime,
+    commitTime,
+    interactions,
+  ) => {
+    console.log(phases, phase)
+    if (!phases || phases.includes(phase)) {
+      queue.push({
+        metadata,
+        id,
+        phase,
+        actualDuration,
+        baseDuration,
+        startTime,
+        commitTime,
+        interactions,
+      })
+    }
+  }
+
+  return <React.Profiler onRender={reportProfile} {...props} />
+}
+
+export {Profiler}
